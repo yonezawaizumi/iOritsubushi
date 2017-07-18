c2d9734ef3e41968fd9149d100a87497a48761d7
yonezawaizumi <izumi@nurs.or.jp>
2017-07-18 09:44:19 +0900

    駅データベースv118

--- Oritsubushi/DatabaseVersion.h
+++ Oritsubushi/DatabaseVersion.h
@@ -9,7 +9,7 @@
 #ifndef Oritsubushi_DatabaseVersion_h
 #define Oritsubushi_DatabaseVersion_h
 
-#define DATABASE_USER_VERSION           116
+#define DATABASE_USER_VERSION           118
 
 typedef struct Duplicater {
     int version;
