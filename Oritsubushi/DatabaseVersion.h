64140dea4ae6df6e0ea9d50cb4459aa8ae4e9bc7
yonezawaizumi <izumi@nurs.or.jp>
2017-10-19 07:36:08 +0900

    データベースv120, iOS 11対応のライブラリー更新

--- Oritsubushi/DatabaseVersion.h
+++ Oritsubushi/DatabaseVersion.h
@@ -9,7 +9,7 @@
 #ifndef Oritsubushi_DatabaseVersion_h
 #define Oritsubushi_DatabaseVersion_h
 
-#define DATABASE_USER_VERSION           118
+#define DATABASE_USER_VERSION           120
 
 typedef struct Duplicater {
     int version;
